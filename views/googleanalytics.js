1276562c0544b3e29f2ca6fd79635bf2b219a3e6
scottdowne <scott.downe@gmail.com>
2014-03-10 12:00:25 -0400

    updates

--- views/googleanalytics.js
+++ views/googleanalytics.js
@@ -4,11 +4,4 @@
   {% if GA_DOMAIN %}
   _gaq.push(['_setDomainName', '{{ GA_DOMAIN }}']);
   {% endif %}
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
 {% endif %}
